9fa2142272b955a42159ff86ac6da5a054350425
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-08-08 00:55:36 +0000

    Tree traversal methods

--- vampire.js
+++ vampire.js
@@ -44,28 +44,40 @@ class Vampire {
 
   // Returns the vampire object with that name, or null if no vampire exists with that name
   vampireWithName(name) {
-    
+    if(this.name === name) return this;
+
+    for (let child of this.offspring){
+      const found = child.vampireWithName(name);
+      if(found){
+        return found;
+      }
+    }
+    return null;
   }
 
   // Returns the total number of vampires that exist
   get totalDescendents() {
-    
+    let totalVamps = 0;
+    // This return just first child
+    totalVamps = totalVamps + this.offspring.length;
+    // With for loop we are going into deep
+   for (const vamp of this.offspring){
+     totalVamps = totalVamps + vamp.totalDescendents;      
+    }
+     return totalVamps;
   }
 
   // Returns an array of all the vampires that were converted after 1980
-  get allMillennialVampires() {
-    
-  }
-
-  /** Stretch **/
-
-  // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-
+   get allMillennialVampires() {
+    let vamps = []; 
+    if(this.yearConverted > 1980) {
+      vamps.push(this);
+    } 
+    for(const vamp of this.offspring){
+      const childMillennialVampires = vamp.allMillennialVampires;
+      vamps = vamps.concat(childMillennialVampires)
+    }
+    return vamps;
   }
 }
 
